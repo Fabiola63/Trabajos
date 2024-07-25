de507f0db811f687f8738742d10beae2038f5538
Fabiola63 <fabiale2815@gmail.com>
2024-07-24 21:39:03 -0600

    Add files via upload

---
+++ QUIZ3/src/javaapplication18/QUIZ3.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication18;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class QUIZ3 {
+    String nombresuperchino = null;
+    String alimentos;
+    String bebidas;
+    String higiene;
+    String limpieza;
+        for(int i = 0; i < nombresuperchino.length; i++) {
+            JOptionPane.showInputDialog(null,"Ingrese el nombre del super del chino");
+            int codigoProducto=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el codigo del Producto"));
+            String nombreEmpleado=JOptionPane.showInputDialog(null, "Escriba el nombre del empleado");
+            int cedulaEmpleado=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese la cedula del empleado"));
+            int precioBaseProducto=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el precio base del producto"));
+}       
+        for(int i =0; i < precioBaseProducto; i++) {
+        double precioBaseProducto = 0;
+            if (alimentos == precioBaseProducto * 0.2 + precioBaseProducto) {
+            System.out.println("El precio de los alimentos es:" + alimentos);
+            } else if (bebidas == precioBaseProducto * 0.3 + precioBaseProducto){
+                System.out.println("El precio de los alimentos es:" + bebidas);
+            } else if (higiene == precioBaseProducto * 0.25 + precioBaseProducto){
+                System.out.println("El precio de los alimentos es:" + bebidas);
+            } else if (limpieza == precioBaseProducto * 0.19 + precioBaseProducto){
+                System.out.println("El precio de los alimentos es:" + limpieza);
+    }
+}
+  
